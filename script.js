c7aa941eb3038f82934a5fe49a7ede3aaf4c8bf8
Francisco <franciscovargasmarcal@gmail.com>
2024-10-03 11:33:02 -0300

    Criando array, objeto e map

--- script.js
+++ script.js
@@ -27,8 +27,27 @@ let nome = 'fiap';
 function ola(nome){
     // `` (strings literais ou templater string, ${} chama a variável)
     console.log(`Seja bem vindo a ${nome}`)
-    alert(`Seja bem vindo a ${nome}`)
+    //alert(`Seja bem vindo a ${nome}`)
     document.write(`seja bem vindo a ${nome}`)
 }
 // executando a função ola passando o valor
 ola(nome)
+
+// Arrow Function, a grosso modo é como se fosse a function reduzida
+
+const hello=()=>(console.log("função arrow function"))
+
+hello()
+
+// Array [], Objeto {} e Metodos ()
+
+let jogadores = [
+    {nome:"Huguinho", idade: 17},
+    {nome:"Zezinho", idade: 18},
+    {nome:"Luizinho", idade: 19}
+]
+
+let listarJogadores = jogadores.map(function(item){
+    return item.nome
+})
+console.log(listarJogadores)
