1cbcc03de0890c8c8f3b1a9ef95723346bfa0be2
Francisco <franciscovargasmarcal@gmail.com>
2024-10-03 11:24:15 -0300

    criando função no javascript

--- script.js
+++ script.js
@@ -2,12 +2,33 @@
 
 i = 0;
 for(let i=0; i<10; i++){
-    console.log(i)
+    //console.log(i)
 }
 
 // While
-
+let f = 0;
 while (f < 10){
-    console.log(f)
+    //console.log(f)
     f++
 }
+
+// do while
+let w = 0;
+
+do{
+    //console.log(w)
+    w++;
+}
+while(w < 5);
+
+// funções
+let nome = 'fiap';
+
+function ola(nome){
+    // `` (strings literais ou templater string, ${} chama a variável)
+    console.log(`Seja bem vindo a ${nome}`)
+    alert(`Seja bem vindo a ${nome}`)
+    document.write(`seja bem vindo a ${nome}`)
+}
+// executando a função ola passando o valor
+ola(nome)
